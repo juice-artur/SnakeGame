cf853216e0e70943b0972130803f0b6212d0b86e
juice-artur <asasin.lol10@gmail.com>
2024-06-04 17:32:05 +0300

    feat: setup back to menu button

--- Source/SnakeGame/UI/SG_StartGameWidget.h
+++ Source/SnakeGame/UI/SG_StartGameWidget.h
@@ -6,8 +6,22 @@
 #include "Blueprint/UserWidget.h"
 #include "SG_StartGameWidget.generated.h"
 
+class UButton;
+
 UCLASS()
 class SNAKEGAME_API USG_StartGameWidget : public UUserWidget
 {
     GENERATED_BODY()
+protected:
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UButton> StartGameButton;
+
+    UPROPERTY(EditDefaultsOnly)
+    TSoftObjectPtr<UWorld> GameLevel;
+
+    virtual void NativeOnInitialized() override;
+
+private:
+    UFUNCTION()
+    void OnStartGame();
 };
